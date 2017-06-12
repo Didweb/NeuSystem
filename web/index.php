e7398da6ebfac1fad616adf1f52014be1a3e3263
Eduard Pinuaga <info@did-web.com>
2017-06-12 16:22:18 +0200

    Test_uno_Commit

--- web/index.php
+++ web/index.php
@@ -19,7 +19,7 @@ include $base_dir.'src'.$ds.'calculator.php';
   <body>
     <div class="container">
           <div class="starter-template">
-        <h1>Calculator</h1>
+        <h1>Calculator Con un cambio realizado.</h1>
     <p>
           <?php
             $cal = new calculator();
