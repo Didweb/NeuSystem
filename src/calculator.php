472482d6335a029cd71da27d338d2381a1662f89
Eduard Pinuaga <info@did-web.com>
2017-06-12 14:29:49 +0200

    Initial Commit

---
+++ src/calculator.php
@@ -0,0 +1,36 @@
+<?php
+
+
+/**
+* Eduardo Pinuaga Linares
+*
+* eduardo.pinuaga-linares@elementsystems.de
+*
+* test PHPDocumentator
+*
+*
+* @author Eduardo Pinuaga Linares
+* @author http://www.elementsystems.de
+*
+* @package calculator
+*/
+class calculator {
+
+
+
+  /**
+  * Summe zwei parametere.
+  *
+  * @return integer summer a + b
+  * @param integer $a nummer
+  * @param integer $b nummer
+  */
+  public function summe($a, $b){
+    $r = $a + $b;
+    return $r;
+  }
+
+
+}
+
+ ?>
---
+++ tests/CalculatorTest.php
@@ -0,0 +1,22 @@
+<?php
+ require_once 'src/calculator.php';
+
+ class CalculatorTest extends PHPUnit_Framework_TestCase
+ {
+
+    protected $cal;
+
+    protected function setUp()
+    {
+        $this->cal = new Calculator();
+    }
+
+     /**
+      * @covers test summer
+      */
+     public function testSummer()
+     {
+         $this->assertEquals(4, $this->cal->summe(2, 2));
+     }
+
+}
