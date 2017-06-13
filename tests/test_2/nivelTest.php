47edaf1e588368c32e8e631bd7b9494cc3371012
Eduard Pinuaga <info@did-web.com>
2017-06-13 09:24:06 +0200

    Mas tests

---
+++ tests/test_2/nivelTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+ class nivelTest extends PHPUnit_Framework_TestCase
+ {
+
+
+
+     /**
+      * @covers test summer
+      */
+     public function testDobleFallo2()
+     {
+         $this->assertEquals(6, 6);
+     }
+
+}
