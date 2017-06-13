a5270d1f1c2994cec048a4e903e231190c33cd76
Eduard Pinuaga <info@did-web.com>
2017-06-13 08:42:17 +0200

    Doble Fallo

---
+++ tests/dobleTest.php
@@ -0,0 +1,17 @@
+<?php
+
+
+ class CalculatorTest extends PHPUnit_Framework_TestCase
+ {
+
+
+
+     /**
+      * @covers test summer
+      */
+     public function testDobleFallo()
+     {
+         $this->assertEquals(8, 5);
+     }
+
+}
