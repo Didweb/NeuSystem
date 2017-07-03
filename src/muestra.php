839765b81d16fc7e4f6c9a3b487fdd01d2dbfec5
Eduard Pinuaga <info@did-web.com>
2017-07-03 13:12:02 +0200

    Otra clase

---
+++ src/muestra.php
@@ -0,0 +1,23 @@
+<?php
+/**
+* @author Eduardo Pinuaga Linares <eduardo.pinuaga-linares@elementsystems.de>
+*
+* @package muestra
+*
+*/
+class muestra {
+
+  /**
+  * Probe die texte mit h1.
+  *
+  * @return string text mit h1
+  * @param string $text text
+  */
+  public function muestra_h1($text){
+    $result = "<h1>".$text."</h1>";
+    return $result;
+  }
+
+
+
+ ?>
